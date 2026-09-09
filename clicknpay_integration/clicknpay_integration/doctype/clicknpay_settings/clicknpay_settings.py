62bf52722e47c02b85bd7510bd077fc38bb7200f
KTM <95043922+KossamMusundire@users.noreply.github.com>
2026-09-09 15:55:12 +0200

    Implement transaction validation and payment URL retrieval
    Added validation methods for transaction currency and amount, and a method to get payment URL.

--- clicknpay_integration/clicknpay_integration/doctype/clicknpay_settings/clicknpay_settings.py
+++ clicknpay_integration/clicknpay_integration/doctype/clicknpay_settings/clicknpay_settings.py
@@ -1,5 +1,19 @@
+import frappe
 from frappe.model.document import Document
 
-
 class ClicknPaySettings(Document):
-    pass
+    
+    def validate_transaction_currency(self, currency):
+        if currency not in ["USD", "ZWG", "ZiG", "ZAR","ZMW"]:
+            frappe.throw(f"Currency {currency} not supported")
+    
+    def validate_transaction_amount(self, currency, amount=None):
+        # ERPNext v16 calls with (currency, amount) or (amount)
+        if amount is None:
+            amount = currency
+        if amount <= 0:
+            frappe.throw("Amount must be > 0")
+    
+    def get_payment_url(self, **kwargs):
+        from clicknpay_integration.clicknpay_integration.api import get_payment_url
+        return get_payment_url(**kwargs)
